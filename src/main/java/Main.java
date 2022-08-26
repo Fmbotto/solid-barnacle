01ecc344aff77a6fd8061ef8ee48c7066c22e428
Fmbotto <111916386+Fmbotto@users.noreply.github.com>
2022-08-26 08:35:14 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class Main {
     @RequestMapping("/")
     String home() {
-        return "Mi primer api felix botto mi primer reto ";
+        return "Mi primer api felix botto mi primer azure ";
     }
     
     @RequestMapping("/prueba")
